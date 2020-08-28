532452c16a0beaeadb4319927cc2c33e106b6b95
Skordilis <Erotokritos.Skordilis@nrel.gov>
2020-08-28 10:54:41 -0600

    updated def for NN training

--- molecule_builder/training.py
+++ molecule_builder/training.py
@@ -2,11 +2,42 @@ import pickle
 import numpy as np
 import random
 
-if __name__ == "__main__":
+from config import AlphaZeroConfig
+from network import Network
 
-    sample = random.sample(range(10),2)
-    
+CONFIG = AlphaZeroConfig()
+""""""
+def sample():
+
+    # Get last N pickled elements and append them to a list
+    sample = np.arange(CONFIG.num_rollouts)[-CONFIG.batch_size:]
+    game_list = []
+
     for i in sample:
         with open('game_{}.pickle'.format(i), 'rb') as f:
             new_data = pickle.load(f)
-            print(new_data)
+            game_list.append(new_data)
+    move_sum = float(sum(len(g["mol_smiles"]) for g in game_list))
+    games = np.random.choice(
+                game_list,
+                size=CONFIG.batch_size,
+                p=[len(g["mol_smiles"]) / move_sum for g in game_list]
+            )
+    game_pos = [(g, np.random.randint(len(g["mol_smiles"])-1)) for g in games]
+
+    mol = [z["network_inputs"]["mol"][i] for (z, i) in game_pos]
+    next_mols = [z["network_inputs"]["next_mols"][i] for (z, i) in game_pos]
+    action_mask = [z["network_inputs"]["action_mask"][i] for (z, i) in game_pos]
+
+    v = [z["reward"] for (z, i) in game_pos]
+    pi = [z["network_inputs"]["pi"][i] for (z, i) in game_pos]
+    
+    return mol, next_mols, action_mask, v, pi
+
+def model_training(network):
+    mol, next_mols, action_mask, v, pi = sample()
+    network.compile()
+    result = network.model.train_on_batch([mol, next_mols, action_mask], [v, pi])
+
+if __name__ == "__main__":
+    mol, next_mols, action_mask, v, pi = sample()
