cd20bb60240c7807a6b36a9ee72ace30ac182857
Skordilis <Erotokritos.Skordilis@nrel.gov>
2020-09-02 11:07:52 -0600

    updated

--- molecule_builder/run_rollout_workers.sh
+++ molecule_builder/run_rollout_workers.sh
@@ -1,33 +1,7 @@
 #! /bin/bash
-<< 'Information'
- Function conda_auto_env enables the user to automatically activate an environment once they enter into
- a project directory, assuming an <environment_name>.yaml already exists in the directory. If that is not
- the case, then conda_auto_env will automatically create one based on the existing yaml file.
+# Run rollout.py on multiple workers
 
- function conda_auto_env() {
-  if [ -e "molecule_environment.yml" ]; then
-    # echo "molecule_environment.yml file found"
-    ENV=$(head -n 1 molecule_environment.yml | cut -f2 -d ' ')
-    # Check if you are already in the environment
-    if [[ $PATH != *$ENV* ]]; then
-      # Check if the environment exists
-      conda activate $ENV
-      if [ $? -eq 0 ]; then
-        :
-      else
-        # Create the environment and activate
-        echo "Conda env '$ENV' doesn't exist."
-        conda env create -q
-        conda activate $ENV
-      fi
-    fi
-  fi
-}
-
-export PROMPT_COMMAND=conda_auto_env
-Information
-
-conda activate path-to-env-eagle
+conda activate path-to-env
 
 num_workers=2
 
