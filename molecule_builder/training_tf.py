0771b311792ab78e3c8062ebe7845ebbb4b89a75
Skordilis <Erotokritos.Skordilis@nrel.gov>
2020-09-08 12:16:49 -0600

    training using tf dataset

--- molecule_builder/training_tf.py
+++ molecule_builder/training_tf.py
@@ -1,10 +1,10 @@
 import glob
 import os
 import pickle
+import random
 import time
 import numpy as np
 import tensorflow as tf
-from tensorflow.python.framework.ops import disable_eager_execution, enable_eager_execution
 
 from config import AlphaZeroConfig
 from network import Network
@@ -64,32 +64,31 @@ def sample_batch(buffer_dir):
         .filter(get_last_n)\
             .map(recover)
     
+    # Map the dataset, shuffle, and create iterator before sampling
     file_list = file_list.map(ds_map_fn)
-
-    # TODO: The following line is a bottleneck (around 8 seconds), 
-    # so far haven't found a way to avoid transforming MapDataset to list
-    start_time = time.time()
-    game_list = list(file_list)
-    end_time = time.time()
-    print("Time taken for this loop: ",end_time - start_time)
+    file_list = file_list.shuffle(num_samples)
+    iterator = iter(file_list)
     
-    games = np.random.choice(len(game_list), size=CONFIG.batch_size)
-    game_pos = [(g, np.random.randint(len(game_list[g][0]["mol_smiles"].numpy())-1)) for g in games]
+    games = []
+    for _ in range(CONFIG.batch_size):
+        games.append(iterator.get_next())
 
-    mol = [game_list[z][0]["network_inputs"]["mol"][i].numpy() for (z, i) in game_pos]
-    next_mols = [game_list[z][0]["network_inputs"]["next_mols"][i].numpy() for (z, i) in game_pos]
-    action_mask = [game_list[z][0]["network_inputs"]["action_mask"][i].numpy() for (z, i) in game_pos]
+    game_pos = [(g, np.random.randint(len(g[0]["mol_smiles"].numpy())-1)) for g in games]
 
-    pi = [game_list[z][0]["network_inputs"]["pi"][i].numpy() for (z, i) in game_pos]
+    mol = [z[0]["network_inputs"]["mol"][i].numpy() for (z, i) in game_pos]
+    next_mols = [z[0]["network_inputs"]["next_mols"][i].numpy() for (z, i) in game_pos]
+    action_mask = [z[0]["network_inputs"]["action_mask"][i].numpy() for (z, i) in game_pos]
+
+    pi = [z[0]["network_inputs"]["pi"][i].numpy() for (z, i) in game_pos]
 
     # Get ranked reward threshold over the entire buffer
-    rewards = [game_list[z[0]][0]["reward"].numpy() for z in game_pos]
+    rewards = [z[0][0]["reward"].numpy() for z in game_pos]
     r_alpha = np.percentile(rewards, 100.*CONFIG.ranked_reward_alpha)
 
     # Compute the ranked reward for each sampled game
     v = []
     for z in game_pos:
-        value = game_list[z[0]][0]["reward"].numpy()
+        value = z[0][0]["reward"].numpy()
         if value < r_alpha:
             rr = -1.
         elif value > r_alpha:
@@ -103,8 +102,6 @@ def sample_batch(buffer_dir):
 def train_model(network, buffer_dir, model_dir):
     for iteration in range(CONFIG.training_iterations):
         mol, next_mols, action_mask, v, pi = sample_batch(buffer_dir)
-        #for gs in range(CONFIG.gradient_steps_per_batch):
-            #loss = network.model.train_on_batch([mol, next_mols, action_mask], [v, pi])
         checkpoint_filepath = os.path.join(model_dir,'cp.ckpt')
         cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath,
                                                             save_weights_only=True,
@@ -117,7 +114,6 @@ def train_model(network, buffer_dir, model_dir):
                                     callbacks=[cp_callback],
                                     verbose=1)
         network.model.save_weights(checkpoint_filepath)
-        #network.model.save(os.path.join(model_dir,'model_{}.h5'.format(time.strftime("%Y%m%d-%H%M%S"))))
 
 if __name__ == "__main__":
 
