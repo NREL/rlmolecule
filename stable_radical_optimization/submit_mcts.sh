2ed5edeac6ad1e945f4b46ee947f0c7f245467bf
erskordi <erskordi@gmail.com>
2020-10-01 14:18:47 -0600

    fixed path of .out file

--- stable_radical_optimization/submit_mcts.sh
+++ stable_radical_optimization/submit_mcts.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=debug
 #SBATCH -n 4
 #SBATCH -c 18
-#SBATCH --output=/scratch/eskordil/git-repos/rlmolecule/mcts.%j.out
+#SBATCH --output=/scratch/eskordil/git-repos/rlmolecule_new/rlmolecule/mcts.%j.out
 
 source ~/.bashrc
 conda activate /projects/rlmolecule/pstjohn/envs/tf2_cpu
