74e9e55830f853b3c9690adb86c01b89bbaf6195
Peter St. John <peterc.stjohn@gmail.com>
2020-10-08 13:53:37 -0600

    updating TF serving example

--- tf_serving_example/run_tf_serving.sh
+++ tf_serving_example/run_tf_serving.sh
@@ -3,7 +3,7 @@
 module unload
 unset LD_PRELOAD
 
-module load singularity-container/3.2.1
+module load singularity-container/3.6.1
 
 SINGULARITYENV_MODEL_NAME=ysi_model singularity exec -B ./ysi_model:/models/ysi_model /projects/rlmolecule/pstjohn/containers/tensorflow-serving.simg tf_serving_entrypoint.sh
 
