486c0c0f024af0ad617d244e53ad2de59822e166
Biagioni <dbiagon@nrel.gov>
2021-03-22 10:22:57 -0600

    factor out the weirdness in multiple inheritance

--- rlmolecule/gym/gym_problem.py
+++ rlmolecule/gym/gym_problem.py
@@ -14,26 +14,30 @@ class MCTSGymProblem(MCTSProblem):
     via the gym interface and gym state."""
     
     def __init__(self,
-                 engine: sqlalchemy.engine.Engine,
                  env: AlphaZeroGymEnv,
                  **kwargs) -> None:
         self.env = deepcopy(env)
-        super().__init__(engine=engine, **kwargs)
+        super().__init__(**kwargs)
 
     def get_initial_state(self) -> GymEnvState:
         _ = self.env.reset()
         return GymEnvState(self.env, 0, 0., 0., False)
 
 
-class TFAlphaZeroGymProblem(MCTSGymProblem, AlphaZeroProblem):
+class AlphaZeroGymProblem(AlphaZeroProblem):
     def __init__(self,
                  engine: sqlalchemy.engine.Engine,
                  env: AlphaZeroGymEnv,
                  **kwargs) -> None:
-        super().__init__(engine=engine, env=env, **kwargs)
+        self.env = deepcopy(env)
+        super().__init__(**kwargs)
+
+    def get_initial_state(self) -> GymEnvState:
+        _ = self.env.reset()
+        return GymEnvState(self.env, 0, 0., 0., False)
 
 
-class TFAlphaZeroGymProblem(MCTSGymProblem, TFAlphaZeroProblem):
+class TFAlphaZeroGymProblem(TFAlphaZeroProblem):
     def __init__(self,
                  engine: sqlalchemy.engine.Engine,
                  env: AlphaZeroGymEnv,
