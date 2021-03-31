0869bf8a552bf3b832b9aae7c097fe6a6b7db964
Jeff Law <jlaw@nrel.gov>
2021-03-31 14:22:46 -0600

    Fixed issue where bash variables were treated literallylly

--- examples/stable_radical_optimization/submit_combined.sh
+++ examples/stable_radical_optimization/submit_combined.sh
@@ -2,6 +2,8 @@
 #SBATCH --account=rlmolecule
 #SBATCH --time=4:00:00
 #SBATCH --job-name=stable_rad_opt
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=jlaw@nrel.gov
 # --- Policy Trainer ---
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:2
@@ -12,14 +14,16 @@
 export WORKING_DIR=/scratch/${USER}/rlmolecule/stable_radical_optimization/
 export START_POLICY_SCRIPT="$SLURM_SUBMIT_DIR/$JOB/.policy.sh"
 export START_ROLLOUT_SCRIPT="$SLURM_SUBMIT_DIR/$JOB/.rollout.sh"
+export PYTHONPTATH="/home/jlaw/projects/arpa-e/rlmolecule_fork:$PYTHONPTATH"
 
 model_dir="/projects/rlmolecule/pstjohn/models/"; 
 stability_model="$model_dir/20210214_radical_stability_new_data/"
-redox_model="$model_dir/20210214_radical_stability_new_data/"
-bde_model="$model_dir/20210214_radical_stability_new_data/"
+redox_model="$model_dir/20210214_redox_new_data/"
+bde_model="$model_dir/20210216_bde_new_nfp/"
 
-cat << "EOF" > "$START_POLICY_SCRIPT"
+cat << EOF > "$START_POLICY_SCRIPT"
 #!/bin/bash
+export PYTHONPTATH="/home/jlaw/projects/arpa-e/rlmolecule_fork:$PYTHONPTATH"
 source /nopt/nrel/apps/anaconda/5.3/etc/profile.d/conda.sh; 
 conda activate /projects/rlmolecule/pstjohn/envs/tf2_gpu
 python -u stable_radical_opt.py --train-policy \
@@ -28,8 +32,9 @@ python -u stable_radical_opt.py --train-policy \
     --bde-model="$bde_model" 
 EOF
 
-cat << "EOF" > "$START_ROLLOUT_SCRIPT"
+cat << EOF > "$START_ROLLOUT_SCRIPT"
 #!/bin/bash
+export PYTHONPTATH="/home/jlaw/projects/arpa-e/rlmolecule_fork:$PYTHONPTATH"
 source /nopt/nrel/apps/anaconda/5.3/etc/profile.d/conda.sh; 
 conda activate /projects/rlmolecule/pstjohn/envs/tf2_cpu
 python -u stable_radical_opt.py --rollout \
