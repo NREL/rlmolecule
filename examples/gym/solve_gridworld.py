ad8db81a3baa012e14a55a53eb1fbd256ff1305e
Peter St. John <peterc.stjohn@gmail.com>
2021-04-26 11:49:54 -0600

    some code cleanup and inheritance fixing

--- examples/gym/solve_gridworld.py
+++ examples/gym/solve_gridworld.py
@@ -1,22 +1,17 @@
-from examples.gym.gridworld_env import policy
 import logging
-import time
 import sys
+import time
 from typing import Tuple
 
 import numpy as np
 from sqlalchemy import create_engine
 
-from rlmolecule.tree_search.reward import Reward
+from gridworld_env import GridWorldEnv as GridEnv, make_empty_grid
 from rlmolecule.alphazero.tfalphazero_problem import TFAlphaZeroProblem
+from rlmolecule.gym.alphazero_gym import AlphaZeroGymEnv
 from rlmolecule.gym.gym_problem import GymProblem
 from rlmolecule.gym.gym_state import GymEnvState
-from rlmolecule.gym.alphazero_gym import AlphaZeroGymEnv
-
-from tf_model import scalar_obs_policy #policy_model_2
-
-from gridworld_env import GridWorldEnv as GridEnv
-from gridworld_env import make_empty_grid
+from tf_model import scalar_obs_policy  # policy_model_2
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -25,11 +20,6 @@ logger = logging.getLogger(__name__)
 # NOTE: These class definitions need to stay outside of construct_problem
 # or you will error out on not being able to pickle/serialize them.
 class GridWorldEnv(AlphaZeroGymEnv):
-    def __init__(self,
-                 configured_env: AlphaZeroGymEnv,
-                 **kwargs):
-        super().__init__(configured_env, **kwargs)
-
     def reset(self):
         return self.env.reset()
 
@@ -38,29 +28,17 @@ class GridWorldEnv(AlphaZeroGymEnv):
 
 
 class GridWorldProblem(GymProblem, TFAlphaZeroProblem):
-    def __init__(self, *,
-                 env: GridWorldEnv,
-                 engine: "sqlalchemy.engine.Engine",
-                 reward_class: Reward,
-                 **kwargs) -> None:
-        super().__init__(reward_class=reward_class, engine=engine, env=env, **kwargs)
-
     def policy_model(self) -> "tf.keras.Model":
-
         return scalar_obs_policy(hidden_layers=2, hidden_dim=256)
 
     def get_policy_inputs(self, state: GymEnvState) -> dict:
-        return {
-            "obs": state.env.get_obs(),
-            "steps": 0.*np.array([np.float64(self.env.episode_steps)])
-        }
+        return {"obs": state.env.get_obs(), "steps": 0. * np.array([np.float64(self.env.episode_steps)])}
 
     def get_reward(self, state: GymEnvState) -> Tuple[float, dict]:
         return state.cumulative_reward, {}
 
 
 def construct_problem(size):
-
     from rlmolecule.tree_search.reward import RankedRewardFactory
 
     # engine = create_engine(f'sqlite:///gridworld_data.db',
@@ -81,35 +59,30 @@ def construct_problem(size):
     engine = create_engine(engine_str, execution_options={"isolation_level": "AUTOCOMMIT"})
 
     run_id = "gridworld_{}".format(size)
-    #policy_checkpoint_dir = "{}_policy_checkpoints".format(run_id)
-    #logger.info("run_id={}, policy_checkpoint_dir={}".format(run_id, policy_checkpoint_dir))
+    # policy_checkpoint_dir = "{}_policy_checkpoints".format(run_id)
+    # logger.info("run_id={}, policy_checkpoint_dir={}".format(run_id, policy_checkpoint_dir))
 
-    reward_factory = RankedRewardFactory(
-            engine=engine,
-            run_id=run_id,
-            reward_buffer_min_size=64,
-            reward_buffer_max_size=200,
-            ranked_reward_alpha=0.75
-    )
+    reward_factory = RankedRewardFactory(engine=engine,
+                                         run_id=run_id,
+                                         reward_buffer_min_size=64,
+                                         reward_buffer_max_size=200,
+                                         ranked_reward_alpha=0.75)
 
     grid = make_empty_grid(size=size)
     env = GridEnv(grid, sparse_rewards=True, use_grayscale_obs=True)
 
-    problem = GridWorldProblem(
-        env=env,
-        engine=engine,
-        reward_class=reward_factory,
-        run_id=run_id,
-        min_buffer_size=64,
-        max_buffer_size=200,
-        batch_size=32
-    )
+    problem = GridWorldProblem(env=env,
+                               engine=engine,
+                               reward_class=reward_factory,
+                               run_id=run_id,
+                               min_buffer_size=64,
+                               max_buffer_size=200,
+                               batch_size=32)
 
     return problem
 
 
 def run_games(size, use_mcts=False, num_mcts_samples=64, num_games=None, seed=None):
-
     np.random.seed(seed)
 
     if use_mcts:
@@ -126,19 +99,17 @@ def run_games(size, use_mcts=False, num_mcts_samples=64, num_games=None, seed=No
         path, reward = game.run(num_mcts_samples=num_mcts_samples)
         elapsed = time.time() - start_time
         print("Seed {} | REWARD: {}   ".format(seed, reward.__dict__))
-        logger.info(('Seed {} | Game Finished -- Reward {:.3f}'.format(seed, reward.raw_reward) +
-                      #' -- Final state {}'.format(path[-1]) +
-                      ' -- CPU time {:1.3f} (s)'.format(elapsed)))
+        logger.info((
+            'Seed {} | Game Finished -- Reward {:.3f}'.format(seed, reward.raw_reward) +
+            # ' -- Final state {}'.format(path[-1]) +
+            ' -- CPU time {:1.3f} (s)'.format(elapsed)))
+
 
 def train_model(size):
-    construct_problem(size).train_policy_model(
-        steps_per_epoch=100,
-        game_count_delay=10,
-        verbose=2)
+    construct_problem(size).train_policy_model(steps_per_epoch=100, game_count_delay=10, verbose=2)
 
 
 def monitor(size):
-
     from rlmolecule.sql.tables import RewardStore
     problem = construct_problem(size=size)
 
@@ -157,25 +128,27 @@ def monitor(size):
 
 
 def setup_argparser():
-    
-
     return parser
 
 
 if __name__ == "__main__":
 
     import argparse
+
     parser = argparse.ArgumentParser(
         description='Solve the Hallway problem (move from one side of the hallway to the other). ' +
-                'Default is to run multiple games and training using multiprocessing')
+        'Default is to run multiple games and training using multiprocessing')
 
     parser.add_argument("--size", type=int, default=8)
-    parser.add_argument('--train-policy', action="store_true", default=False,
+    parser.add_argument('--train-policy',
+                        action="store_true",
+                        default=False,
                         help='Train the policy model only (on GPUs)')
-    parser.add_argument('--rollout', action="store_true", default=False,
+    parser.add_argument('--rollout',
+                        action="store_true",
+                        default=False,
                         help='Run the game simulations only (on CPUs)')
-    parser.add_argument('--num-workers', type=int, default=3,
-                        help='Number of multiprocessing workers')
+    parser.add_argument('--num-workers', type=int, default=3, help='Number of multiprocessing workers')
     parser.add_argument("--num-games", type=int, default=None)
     parser.add_argument("--num-mcts-samples", type=int, default=50)
     parser.add_argument("--use-mcts", action="store_true")
@@ -188,30 +161,26 @@ if __name__ == "__main__":
     if args.train_policy:
         train_model(args.size)
     elif args.rollout:
-        run_games(
-            size=args.size,
-            use_mcts=args.use_mcts,
-            num_mcts_samples=args.num_mcts_samples,
-            num_games=args.num_games)
+        run_games(size=args.size,
+                  use_mcts=args.use_mcts,
+                  num_mcts_samples=args.num_mcts_samples,
+                  num_games=args.num_games)
     else:
         assert args.num_workers >= 3  # need at least 3 workers here...
 
         import multiprocessing
 
-        jobs = [multiprocessing.Process(target=monitor, args=(args.size,))]
+        jobs = [multiprocessing.Process(target=monitor, args=(args.size, ))]
         jobs[0].start()
         time.sleep(1)
 
         for i in range(args.num_workers - 2):
             seed = np.random.randint(99999999)
             jobs += [
-                multiprocessing.Process(
-                    target=run_games,
-                    args=(args.size, args.use_mcts, args.num_mcts_samples, seed)
-                )
+                multiprocessing.Process(target=run_games, args=(args.size, args.use_mcts, args.num_mcts_samples, seed))
             ]
 
-        jobs += [multiprocessing.Process(target=train_model, args=(args.size,))]
+        jobs += [multiprocessing.Process(target=train_model, args=(args.size, ))]
 
         for job in jobs[1:]:
             job.start()
