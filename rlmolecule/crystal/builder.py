5a29ab06476bafb90fe7c25ca6d934ef0a9f9357
Jeff Law <jlaw@nrel.gov>
2022-02-03 12:06:30 -0700

    Add files where problematic prototypes are removed i.e., no overlapping atom sites

--- rlmolecule/crystal/builder.py
+++ rlmolecule/crystal/builder.py
@@ -8,8 +8,14 @@ from rlmolecule.crystal.crystal_state import CrystalState
 
 logger = logging.getLogger(__name__)
 dir_path = os.path.dirname(os.path.realpath(__file__))
-action_graph_file = os.path.join(dir_path, 'inputs', 'elements_to_compositions_lt50atoms.edgelist.gz')
-action_graph2_file = os.path.join(dir_path, 'inputs', 'comp_type_to_decorations_lt50atoms.edgelist.gz')
+# elements to compositions
+action_graph_file = os.path.join(dir_path,
+                                 'inputs',
+                                 'eles_to_comps_lt50atoms_lt100dist.edgelist.gz')
+# comp_type to decorations
+action_graph2_file = os.path.join(dir_path,
+                                  'inputs',
+                                  'comp_type_to_decors_lt50atoms_lt100dist.edgelist.gz')
 
 
 class CrystalBuilder:
