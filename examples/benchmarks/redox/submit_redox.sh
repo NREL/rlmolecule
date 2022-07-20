f938476ca498d5bcace01bccf134b621143cce19
Shree Sowndarya Santhanalakkshmi Vejaykummar <svss@el1.ib0.cm.hpc.nrel.gov>
2022-07-20 12:21:13 -0600

    submit script

---
+++ examples/benchmarks/redox/submit_redox.sh
@@ -0,0 +1,13 @@
+#!/bin/bash --login
+#SBATCH --account=rlmolecule
+#SBATCH --job-name=redox
+#SBATCH --partition=debug
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=36
+
+module load cudnn/8.1.1/cuda-11.2
+conda activate /projects/rlmolecule/svss/envs/rllib
+python run_redox.py
