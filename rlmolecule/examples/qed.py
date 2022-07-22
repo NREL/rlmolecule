e0b67934887885747e224eb18d672d6fa7bb098a
Peter St. John <peterc.stjohn@gmail.com>
2022-07-22 14:35:12 -0600

    restructing some of the key docstrings

--- rlmolecule/examples/qed.py
+++ rlmolecule/examples/qed.py
@@ -1,12 +1,5 @@
 from rdkit.Chem.QED import qed
-from rlmolecule.actors import CSVActorWriter
 from rlmolecule.molecule_state import MoleculeState
-
-
-def get_csv_logger(filename):
-    return CSVActorWriter.options(
-        name="qed_results", lifetime="detached", get_if_exists=True
-    ).remote(filename)
 
 
 class QEDState(MoleculeState):
